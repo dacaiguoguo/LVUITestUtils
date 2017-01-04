210445e092291291067647fb3de11f862d29f6d9
sunyanguo <sunyanguo@lvmama.com>
2017-01-04 16:25:55 +0800

    endEditing add

--- Example/LVUITestUtils/LVSecondViewController.m
+++ Example/LVUITestUtils/LVSecondViewController.m
@@ -18,10 +18,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
 }
 
 - (IBAction)action:(id)sender {
+    [self.view endEditing:YES];
     UIAlertController *alertVc = [UIAlertController alertControllerWithTitle:@"Message" message:self.textField.text preferredStyle:(UIAlertControllerStyleActionSheet)];
     alertVc.popoverPresentationController.sourceRect = self.alertButton.bounds;
     alertVc.popoverPresentationController.sourceView = self.alertButton;
